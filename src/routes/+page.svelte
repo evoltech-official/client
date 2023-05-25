3b4684dee5476bb3b22ede32da15336069415d92
andreilivshits <andreilivshits@gmail.com>
2023-05-25 23:31:04 +0000

    half commint

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import '../lib/internal/main.js';
+    var term;
+    var winwidth;
+    var winheight;
+    var evol = new Evol(winheight,winwidth);
+    evol.start(term);
+    import { io } from "socket.io-client";
+    import Evol from '../lib/internal/main.js';
+</script>
+
+<svelte:window bind:innerHeight={winheight} bind:innerWidth={winwidth}></svelte:window>
+<pre class="terminalscreen" id="term" bind:this={term}>
+    
+</pre>
